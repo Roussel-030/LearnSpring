2a201240412b8ff6d7934159708f650732aab33a
Roussel-030 <yvanroussel31@gmail.com>
2023-09-04 11:19:11 +0300

    Create project quickstart with maven

---
+++ invoise/src/main/java/com/myCompany/invoise/App.java
@@ -0,0 +1,13 @@
+package com.myCompany.invoise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
