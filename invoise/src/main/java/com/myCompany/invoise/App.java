11f544b6555625fd6197179849a29b0e9ab09936
Roussel-030 <yvanroussel31@gmail.com>
2023-09-07 18:04:29 +0300

    Change App.java in class of configuration

--- invoise/src/main/java/com/myCompany/invoise/App.java
+++ invoise/src/main/java/com/myCompany/invoise/App.java
@@ -3,18 +3,22 @@ package com.myCompany.invoise;
 import com.myCompany.invoise.controller.InvoiceControllerInterface;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {"com.myCompany.invoise.controller.web","com.myCompany.invoise.service.prefix","com.myCompany.invoise.repository.database"})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
     {
-        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class,AppConfigPropertySource.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
         invoiceControllerInterface.createInvoice();
     }
--- invoise/src/main/java/com/myCompany/invoise/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.myCompany.invoise;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"com.myCompany.invoise.controller.web","com.myCompany.invoise.service.prefix","com.myCompany.invoise.repository.database"})
-public class AppConfig {
-}
